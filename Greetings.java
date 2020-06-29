e88a3db43fbc6beee689d46e2126b8f98e40f5ca
Husnah-The-Programmer <67584836+Husnah-The-Programmer@users.noreply.github.com>
2020-06-29 04:49:35 -0700

    add real java file of our application

---
+++ Greetings.java
@@ -0,0 +1,9 @@
+
+public class Greetings {
+     public static void main (String []args) {
+    	 /* println() function to write Greetings! */
+    	 System.out.println("Greetings!");
+    	 System.out.println("Hello World!");
+    	 System.out.println("Good day!");
+     }
+}
